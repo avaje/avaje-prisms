e49371358b68d49eb8148516a3d297d9eed3d0a5
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-03-11 21:36:28 -0500

    only collectList at the end

--- avaje-prisms/src/main/java/io/avaje/prism/internal/FactoryMethodWriter.java
+++ avaje-prisms/src/main/java/io/avaje/prism/internal/FactoryMethodWriter.java
@@ -46,9 +46,7 @@ public class FactoryMethodWriter {
 
   private void writeIsPresent() {
     // Is Present
-    out.format(
-        "%s    /** Returns true if the prism annotation is present on the element, else false. */%n",
-        indent);
+    out.format("%s    /** Returns true if the prism annotation is present on the element, else false. */%n", indent);
     out.format("%s    %sstatic boolean isPresent(Element element) {%n", indent, access);
     out.format("%s        return getInstanceOn(element) != null;%n", indent);
     out.format("%s   }%n%n", indent);
@@ -56,15 +54,9 @@ public class FactoryMethodWriter {
 
   private void writeGetInstanceOn() {
     // get single instance
-    out.format(
-        "%s    /** Return a prism representing the {@code @%s} annotation on 'e'. %n",
-        indent, annName);
-    out.format(
-        "%s      * similar to {@code element.getAnnotation(%s.class)} except that %n",
-        indent, annName);
-    out.format(
-        "%s      * an instance of this class rather than an instance of {@code %s}%n",
-        indent, annName);
+    out.format("%s    /** Return a prism representing the {@code @%s} annotation on 'e'. %n", indent, annName);
+    out.format("%s      * similar to {@code element.getAnnotation(%s.class)} except that %n", indent, annName);
+    out.format("%s      * an instance of this class rather than an instance of {@code %s}%n", indent, annName);
     out.format("%s      * is returned.%n", indent);
     out.format("%s      */%n", indent);
     out.format("%s    %sstatic %s getInstanceOn(Element element) {%n", indent, access, name);
@@ -77,19 +69,12 @@ public class FactoryMethodWriter {
   void writeGetOptionalOn() {
 
     // getOptionalOn
-    out.format(
-        "%s    /** Return a Optional representing a nullable {@code @%s} annotation on 'e'. %n",
-        indent, annName);
-    out.format(
-        "%s      * similar to {@code element.getAnnotation(%s.class)} except that %n",
-        indent, annName);
-    out.format(
-        "%s      * an Optional of this class rather than an instance of {@code %s}%n",
-        indent, annName);
+    out.format("%s    /** Return a Optional representing a nullable {@code @%s} annotation on 'e'. %n", indent, annName);
+    out.format("%s      * similar to {@code element.getAnnotation(%s.class)} except that %n", indent, annName);
+    out.format("%s      * an Optional of this class rather than an instance of {@code %s}%n", indent, annName);
     out.format("%s      * is returned.%n", indent);
     out.format("%s      */%n", indent);
-    out.format(
-        "%s    %sstatic Optional<%s> getOptionalOn(Element element) {%n", indent, access, name);
+    out.format("%s    %sstatic Optional<%s> getOptionalOn(Element element) {%n", indent, access, name);
     out.format("%s        AnnotationMirror mirror = getMirror(PRISM_TYPE, element);%n", indent);
     out.format("%s        if(mirror == null) return Optional.empty();%n", indent);
     out.format("%s        return getOptional(mirror);%n", indent);
@@ -103,55 +88,46 @@ public class FactoryMethodWriter {
     out.format("%s      */%n", indent);
     out.format("%s    %sstatic List<%s> getAllOnMetaAnnotations(Element element) {%n",indent, access, name);
     out.format("%s        if (element == null || element.getAnnotationMirrors().isEmpty()) return List.of();%n%n",indent);
-    out.format("%s        return getAllOnMetaAnnotations(element, new HashSet<>());%n", indent);
+    out.format("%s        //use a hashset to keep track of seen annotations %n", indent);
+    out.format("%s        return getAllOnMetaAnnotations(element, new HashSet<>()).collect(toList());%n", indent);
     out.format("%s   }%n%n", indent);
-    //this will prevent a recursive loop
-    out.format("%s    private static List<%s> getAllOnMetaAnnotations(Element element, Set<String> seen) {%n",indent, name);
-    out.format("%s          if (element == null || element.getAnnotationMirrors().isEmpty()) return List.of();%n%n",indent);
+
+
+    out.format("%s    /** Recursively search annotations elements for prisms.%n", indent);
+    out.format("%s      * Uses a set to keep track of known annotations to avoid repeats/recursive loop. %n",indent);
+    out.format("%s      */%n", indent);
+    out.format("%s    private static Stream<%s> getAllOnMetaAnnotations(Element element, Set<String> seen) {%n",indent, name);
+    out.format("%s          if (element == null || element.getAnnotationMirrors().isEmpty()) return Stream.of();%n%n",indent);
     out.format("%s        return element.getAnnotationMirrors().stream()%n", indent);
     out.format("%s            .map(AnnotationMirror::getAnnotationType)%n", indent);
+    out.format("%s            //only search annotations %n", indent);
     out.format("%s            .filter(t -> seen.add(t.toString()))%n", indent);
     out.format("%s            .map(DeclaredType::asElement)%n", indent);
     out.format("%s            .flatMap(%n", indent);
     out.format("%s                e ->%n", indent);
     out.format("%s                    Stream.concat(%n", indent);
-    out.format("%s                        getAllOnMetaAnnotations(e, seen).stream(),%n", indent);
-    out.format("%s                        getMirrors(PRISM_TYPE, element).stream().map(%s::getInstance)))%n",indent, name);
-    out.format("%s            .collect(toList());%n", indent);
+    out.format("%s                        getAllOnMetaAnnotations(e, seen),%n", indent);
+    out.format("%s                        getMirrors(PRISM_TYPE, element).stream().map(%s::getInstance)));%n", indent, name);
     out.format("%s   }%n%n", indent);
   }
 
   private void writeGetAllInstances() {
     // get multiple instances
-    out.format(
-        "%s    /** Return a list of prisms representing the {@code @%s} annotation on 'e'. %n",
-        indent, annName);
-    out.format(
-        "%s      * similar to {@code e.getAnnotationsByType(%s.class)} except that %n",
-        indent, annName);
-    out.format(
-        "%s      * instances of this class rather than instances of {@code %s}%n", indent, annName);
+    out.format("%s    /** Return a list of prisms representing the {@code @%s} annotation on 'e'. %n",indent, annName);
+    out.format("%s      * similar to {@code e.getAnnotationsByType(%s.class)} except that %n", indent, annName);
+    out.format("%s      * instances of this class rather than instances of {@code %s}%n", indent, annName);
     out.format("%s      * is returned.%n", indent);
     out.format("%s      */%n", indent);
-    out.format(
-        "%s    %sstatic List<%s> getAllInstancesOn(Element element) {%n", indent, access, name);
-    out.format(
-        "%s        return getMirrors(PRISM_TYPE, element).stream().map(%s::getInstance).collect(toList());%n",
-        indent, name);
+    out.format("%s    %sstatic List<%s> getAllInstancesOn(Element element) {%n", indent, access, name);
+    out.format("%s        return getMirrors(PRISM_TYPE, element).stream().map(%s::getInstance).collect(toList());%n", indent, name);
     out.format("%s   }%n%n", indent);
   }
 
   private void writeGetInstance() {
-    out.format(
-        "%s    /** Return a prism of the {@code @%s} annotation whose mirror is mirror. %n",
-        indent, annName);
+    out.format("%s    /** Return a prism of the {@code @%s} annotation whose mirror is mirror. %n", indent, annName);
     out.format("%s      */%n", indent);
-    out.format(
-        "%s    %sstatic %s getInstance(AnnotationMirror mirror) {%n",
-        indent, inner ? "private " : access, name);
-    out.format(
-        "%s        if(mirror == null || !PRISM_TYPE.equals(mirror.getAnnotationType().toString())) return null;%n%n",
-        indent);
+    out.format("%s    %sstatic %s getInstance(AnnotationMirror mirror) {%n", indent, inner ? "private " : access, name);
+    out.format("%s        if(mirror == null || !PRISM_TYPE.equals(mirror.getAnnotationType().toString())) return null;%n%n", indent);
     out.format("%s        return new %s(mirror);%n", indent, name);
     out.format("%s    }%n%n", indent);
   }
@@ -159,22 +135,13 @@ public class FactoryMethodWriter {
   private void writeGetOptional() {
 
     // getOptional
-    out.format(
-        "%s    /** Return a {@code Optional<%s>} representing a {@code @%s} annotation mirror. %n",
-        indent, name, annName);
-    out.format(
-        "%s      * similar to {@code e.getAnnotation(%s.class)} except that %n", indent, annName);
-    out.format(
-        "%s      * an Optional of this class rather than an instance of {@code %s}%n",
-        indent, annName);
+    out.format("%s    /** Return a {@code Optional<%s>} representing a {@code @%s} annotation mirror. %n", indent, name, annName);
+    out.format("%s      * similar to {@code e.getAnnotation(%s.class)} except that %n", indent, annName);
+    out.format("%s      * an Optional of this class rather than an instance of {@code %s}%n", indent, annName);
     out.format("%s      * is returned.%n", indent);
     out.format("%s      */%n", indent);
-    out.format(
-        "%s    %sstatic Optional<%s> getOptional(AnnotationMirror mirror) {%n",
-        indent, inner ? "private " : access, name);
-    out.format(
-        "%s        if(mirror == null || !PRISM_TYPE.equals(mirror.getAnnotationType().toString())) return Optional.empty();%n%n",
-        indent);
+    out.format("%s    %sstatic Optional<%s> getOptional(AnnotationMirror mirror) {%n", indent, inner ? "private " : access, name);
+    out.format("%s        if(mirror == null || !PRISM_TYPE.equals(mirror.getAnnotationType().toString())) return Optional.empty();%n%n", indent);
     out.format("%s        return Optional.of(new %s(mirror));%n", indent, name);
     out.format("%s    }%n%n", indent);
   }
