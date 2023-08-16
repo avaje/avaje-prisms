18a0e5a76b23884913c3f247a1010081b76e7553
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-16 08:39:18 -0400

    Split processor into separate module (#20)
    * split processor into separate module
    
    * resources
    
    * Update module-info.java

--- avaje-prisms/src/main/java/module-info.java
+++ avaje-prisms/src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module io.avaje.prism {
-
-  exports io.avaje.prism;
   requires java.compiler;
-  provides javax.annotation.processing.Processor with io.avaje.prism.internal.PrismGenerator;
+  exports io.avaje.prism;
 
 }
