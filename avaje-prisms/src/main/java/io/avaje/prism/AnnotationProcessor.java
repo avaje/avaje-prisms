c8a2604ba92eab09ac9d0fc9a9901f60da6ff644
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-17 11:00:17 -0400

    Update AnnotationProcessor.java

--- avaje-prisms/src/main/java/io/avaje/prism/AnnotationProcessor.java
+++ avaje-prisms/src/main/java/io/avaje/prism/AnnotationProcessor.java
@@ -7,12 +7,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Dedicated annotation for signaling the prism generator to create an entry in META-INF/services.
+ * Dedicated annotation for signaling the prism generator to create an entry in {@code META-INF/services/javax.annotation.processing.Processor}.
  * The generator will automatically try to write META-INF files if the following annotations are
  * detected on a concrete processor class.
  *
  * <ul>
- *   <li>Any Avaje Prism annotations
+ *   <li>Any avaje prism annotations
  *   <li>{@link javax.annotation.processing.SupportedAnnotationTypes @SupportedAnnotationTypes}
  *   <li>{@link javax.annotation.processing.SupportedOptions @SupportedOptions}
  *   <li>{@link javax.annotation.processing.SupportedSourceVersion @SupportedSourceVersion}
