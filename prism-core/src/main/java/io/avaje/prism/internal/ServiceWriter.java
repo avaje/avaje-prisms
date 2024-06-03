e39eea2067764b0962a1ecd4243d5eb0f43d20b9
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-06-02 23:33:48 -0400

    remove service validation logic

--- prism-core/src/main/java/io/avaje/prism/internal/ServiceWriter.java
+++ prism-core/src/main/java/io/avaje/prism/internal/ServiceWriter.java
@@ -7,13 +7,9 @@ import static io.avaje.prism.internal.APContext.logError;
 import static io.avaje.prism.internal.APContext.logNote;
 import static io.avaje.prism.internal.APContext.logWarn;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -34,27 +30,8 @@ final class ServiceWriter {
   static void write() {
 
     if (services.isEmpty()) return;
-
     // Read the existing service files
     final Filer filer = filer();
-    try (final var f =
-            filer
-                .getResource(StandardLocation.CLASS_OUTPUT, "", "META-INF/services/" + PROCESSOR)
-                .openInputStream();
-        final BufferedReader r =
-            new BufferedReader(new InputStreamReader(f, StandardCharsets.UTF_8)); ) {
-
-      String line;
-      while ((line = r.readLine()) != null) services.add(line);
-    } catch (final FileNotFoundException | java.nio.file.NoSuchFileException x) {
-      // missing and thus not created yet
-    } catch (final IOException x) {
-      logError(
-          "Failed to load existing service definition file. SPI: "
-              + PROCESSOR
-              + " exception: "
-              + x);
-    }
     try {
       logNote("Writing META-INF/services/%s", PROCESSOR);
       final FileObject f =
@@ -89,7 +66,6 @@ final class ServiceWriter {
             if (line.contains("io.avaje.prism.core")) {
               logWarn(module, "io.avaje.prism.core should be not be used directly");
             }
-            continue;
           }
         }
 
